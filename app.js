4c40f57282fbe5b5ab76c7c353ac8a9393fcf908
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-09-12 15:49:06 -0700

    jQuery code isnt working, added eslintrc file

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ new Projects ('Bus Mall', 'https://github.com/Cloud887/bus-mall');
 new Projects ('Tennis Anyone', 'https://github.com/Cloud887/tennis_anyone');
 
 $(document).ready(function() {
-  $('.fa-bars').click(function() {
-    $('section').css('display', 'hidden');
+  $('.fa fa-bars').click(function() {
+    $('.fa fa-bars').css('display', 'none');
   })
 })

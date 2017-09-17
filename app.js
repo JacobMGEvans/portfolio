145932678445c1cdc0551831df4f364c00d8f30d
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-09-17 13:57:00 -0700

    trying to get template to work

--- app.js
+++ app.js
@@ -12,13 +12,26 @@ dataSource.forEach(function(rawData) {
     var projectRender = `<a href= '${rawData.url}'> <p>${rawData.name}</p>  </a>
     ${rawData.description}`
     $('#projectRender').append(projectRender);
-  }else{
+  }else if(rawData.award){
+    awardsArr.push(new Projects(rawData))
+    var awardRender = `<p> ${rawData.award} </p>`
+    $('#awardsRender').append(awardRender)
+  }
+  else{
     educationArr.push(new Projects(rawData));
     var educationRender =  `<p>${rawData.name}</p>
     ${rawData.degree}`
     $('#educationRender').append(educationRender);
   }
 })
+
+var rawTemplateHTML = $('.template').html();
+var compiledTemplate = Handlebars.compile(rawTemplateHTML);
+
+Projects.prototype.toHtml = function (rawData) {
+  var fillHTML = compiledTemplate(this);
+  $('#renderedTemplate').append(fillHTML);
+};
 
 $('.fa-bars').click(function(){
   $('.hide').css('display', 'block');

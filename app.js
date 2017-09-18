a1f6f9483a9c2a4f2aa10df3c112433463f202ff
Hewlette Dibble <hcodydibble@gmail.com>
2017-09-17 17:12:45 -0700

    up to day 4 specs I believe. code is ugly unfortunately but it can be cleaned and I think some can be combined

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
 'use strict';
 
-var projectArr = [], educationArr = [], awardsArr = [];
+var projectArr = [], awardsArr = [];
 
 function Projects(projectDataSource) {
   Object.assign(this, projectDataSource)
 }
 
-//not working with handlebars template
 dataSource.forEach(function(rawData) {
   if(rawData.url){
     projectArr.push(new Projects(rawData))
-    var projectRender = `<a href= '${rawData.url}'> <p>${rawData.name}</p>  </a>
-    ${rawData.description}`
-    $('#projectRender').append(projectRender);
-  }else if(rawData.award){
+  }else {
     awardsArr.push(new Projects(rawData))
-    var awardRender = `<p> ${rawData.award} </p>`
-    $('#awardsRender').append(awardRender)
-  }
-  else{
-    educationArr.push(new Projects(rawData));
-    var educationRender =  `<p>${rawData.name}</p>
-    ${rawData.degree}`
-    $('#educationRender').append(educationRender);
   }
 })
 
-var rawTemplateHTML = $('.template').html();
+var rawTemplateHTML = $('.proTemplate').html();
 var compiledTemplate = Handlebars.compile(rawTemplateHTML);
+var templateHTML = $('.awardTemplate').html();
+var compTemplate = Handlebars.compile(templateHTML);
 
 Projects.prototype.toHtml = function (rawData) {
-  var fillHTML = compiledTemplate(this);
-  $('#renderedTemplate').append(fillHTML);
+  var fillHTML = compiledTemplate(new Projects(this));
+  $('#projectTemplate').append(fillHTML);
+  var fillHtml2 = compTemplate(new Projects(this));
+  $('#awTemp').append(fillHtml2);
 };
 
 $('.fa-bars').click(function(){
   $('.hide').css('display', 'block');
+})
+projectArr.forEach(function(project){
+  $('#projectTemplate').append(project.toHtml());
+})
+awardsArr.forEach(function(award){
+  $('#awTemp').append(award.toHtml());
+})
+$('#projects').click(function(){
+  $('#projectTemplate').toggle();
+})
+$('#awards').click(function(){
+  $('#awTemp').toggle();
 })
--- projects.js
+++ projects.js
@@ -2,13 +2,13 @@
 
 
 var dataSource = [
-{name:`<p>Chocolate Pizza</p>`, url:`'https://github.com/hcodydibble/chocolate-pizza'`, description:`'<p></p>'`},
-{name:`<p>Bus Mall<p>`, url:`"https://github.com/Cloud887/bus-mall"`, description:`<p></p>`},
-{name:`<p>Tennis Anyone<p>`,url:`"https://github.com/Cloud887/tennis_anyone"`, description: `<p></p>`},
-{name:`<p>University of Hawaii Maui</p>`, dates:`<p>2014-2016</p>`, degree:`<p>Associates in Liberal Arts</p>`},
-{award: `<p>Co-Founded Maui Search & Rescue</p>`},
-{award: `<p>SARTECH III Certification</p>`},
-{award: `<p>ASVAB 98/99</p>`},
-{award: `<p>Various Letters of Appreciation And Awards</p>`}
+{name:`Chocolate Pizza`, url:`https://github.com/hcodydibble/chocolate-pizza`, description:`This is a description`},
+{name:`Bus Mall`, url:`https://github.com/Cloud887/bus-mall`, description:`This is a description`},
+{name:`Tennis Anyone`,url:`https://github.com/Cloud887/tennis_anyone`, description: `This is a description`},
+{name:`University of Hawaii Maui`, dates:`2014-2016`, degree:`Associates in Liberal Arts`},
+{award: `Co-Founded Maui Search & Rescue`},
+{award: `SARTECH III Certification`},
+{award: `ASVAB 98/99`},
+{award: `Various Letters of Appreciation And Awards`}
 ]
 //add more education and the awards & achievements

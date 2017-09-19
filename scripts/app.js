c32ab907ab8940a40a6fcf7b9681718b07eb7b4e
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-09-19 12:24:04 -0700

    created AJAX Get func, and a JSON database

--- scripts/app.js
+++ scripts/app.js
@@ -1,44 +1,46 @@
 'use strict';
 
 let projectArr = [], awardsArr = [];
-
-function Projects(projectDataSource) {
-  Object.assign(this, projectDataSource)
-}
-
-dataSource.forEach(function(rawData) {
-  if(rawData.url){
-    projectArr.push(new Projects(rawData))
-  }else {
-    awardsArr.push(new Projects(rawData))
-  }
-})
-
 let rawTemplateHTML = $('.proTemplate').html();
 let compiledTemplate = Handlebars.compile(rawTemplateHTML);
 let templateHTML = $('.awardTemplate').html();
 let compTemplate = Handlebars.compile(templateHTML);
 
+function Projects(projectDataSource) {
+  Object.assign(this, projectDataSource)
+}
+
+
 Projects.prototype.toHtml = function (rawData) {
-  let fillHTML = compiledTemplate(new Projects(this));
+  let fillHTML = compiledTemplate(this);
   $('#projectTemplate').append(fillHTML);
-  let fillHtml2 = compTemplate(new Projects(this));
+  let fillHtml2 = compTemplate(this);
   $('#awTemp').append(fillHtml2);
 };
 
-$('.fa-bars').click(function(){
+$.get('data/biographyData.json',function(databaseData){
+  databaseData.forEach(function(info){
+    (info.url) ? projectArr.push(new Projects(info)) :
+      awardsArr.push(new Projects(info))
+  })
+  projectArr.forEach(function(project){
+    $('#projectTemplate').append(project.toHtml());
+  })
+
+  awardsArr.forEach(function(award){
+    $('#awTemp').append(award.toHtml());
+  })
+})
+
+$('.fa-bars').on('click', function(){
   $('#projectTemplate').toggle();
   $('#awTemp').toggle();
 })
-projectArr.forEach(function(project){
-  $('#projectTemplate').append(project.toHtml());
-})
-awardsArr.forEach(function(award){
-  $('#awTemp').append(award.toHtml());
-})
-$('#projects').click(function(){
+
+$('#projects').on('click', function(){
   $('#projectTemplate').toggle();
 })
-$('#awards').click(function(){
+
+$('#awards').on('click', function(){
   $('#awTemp').toggle();
 })

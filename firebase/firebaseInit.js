9a7e75bacd813c2f1643f51541f879b07894cb27
Jacob M-G Evans <jacobmgevans@gmail.com>
2018-03-18 20:53:00 -0700

    added firebase configs

---
+++ firebase/firebaseInit.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+
+const config = {
+  apiKey: '<API_KEY>',
+  authDomain: '<PROJECT_ID>.firebaseapp.com',
+  databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
+  storageBucket: '<BUCKET>.appspot.com',
+};
+
+firebase.initializeApp(config);
+
+
+export default firebase;

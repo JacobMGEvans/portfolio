4dbabfee2bb3f1b233b54b02716e7eff93865457
Jacob M-G Evans <jacobmgevans@gmail.com>
2018-03-21 15:52:09 -0700

    may use the constructor in the about page, added more logic to the about page

--- frontend/src/components/about/index.js
+++ frontend/src/components/about/index.js
@@ -1,6 +1,25 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react';
+
 import {
   BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+  Route,
+} from 'react-router-dom';
 
+class About extends PureComponent {
+  constructor(props) {
+    super(props);
+    // May use constructor, not sure yet.
+    // something to do with getting twitter streaming in the page.
+    // something to do with getting GitHub on the page
+  }
+
+  // Add Image preferably a few at least one professional headshot.
+  //
+  render() {
+    return (
+      <div>Something about Me</div>
+    );
+  }
+}
+
+export default About;

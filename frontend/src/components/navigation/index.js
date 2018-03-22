3d442e31c8ac184578ae8d16cdb39f6cd22a696e
Jacob M-G Evans <jacobmgevans@gmail.com>
2018-03-22 01:33:59 -0700

    moved index.html and main.js to root

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 
 //Use the spiraling out floating buttons from main button. 
 //
+class Navigation extends PureComponent{
+  
+}
+
+export default Navigation
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import App from './src/components/app';
+import App from './frontend/src/components/app';
 
 //  context api instead of redux?
 

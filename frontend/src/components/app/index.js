91fa89e06a4099219cd1ce928f7719bc2045c886
Jacob M-G Evans <jacobmgevans@gmail.com>
2018-03-24 14:50:50 -0700

    imports for router in APP

--- frontend/src/components/app/index.js
+++ frontend/src/components/app/index.js
@@ -3,14 +3,18 @@ import React, { Component }from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch
 } from 'react-router-dom';
 
 import Landing from '../landing'
-import Switch from 'material-ui';
+import Navigation from '../navigation';
+import Contacts from '../contacts'
+import About from '../about'
 
 class App extends Component {
   render() {
     return (
+
       <div>
       <Navigation />
       <Router>

2be53943af07a93a4b4f1d26a3f0c2931434a04a
Jacob <jacobmgevans@gmail.com>
2018-04-05 21:19:22 -0700

    removed components that were overly complicated going to start from scratch build out most of the homepage in one component then break things apart as needed

--- frontend/src/components/home/index.js
+++
@@ -1,3 +0,0 @@
-//home page that can be scrollable and use either spiralling out navigation button or a nice drop down to skip to those scroll 
-//down sections. The Home page should have the best design while the rest of the routes will be nicely designed 
-//however just minimalist. 

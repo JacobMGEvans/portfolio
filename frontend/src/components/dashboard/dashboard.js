72aea557746c865845ddef3615402016f5248bad
Jacob <jacobmgevans@gmail.com>
2018-04-12 21:20:15 -0700

    added state for future toggle in dashboard and the nav and div for the areas for text bio and headline

--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-class dashboard extends Component{
-    constructor(props, context) {
-        super(props, context);
-        
-    }
+class dashboard extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state({
+      toggled: false,
+    });
+  }
 
- 
- render() {
-     return (
-         <div>
-            <nav>This will be the Nav bar</nav>
-             
-         </div>
-     );
- }
+  render() {
+    return (
+      <div>
+        <nav>This will be the Nav bar</nav>
+
+        <div>Summary and Bio will go here</div>
+      </div>
+    );
+  }
 }
 
-export default dashboard
+export default dashboard;

33fb81d49e44597c67bb3c2027bc841760e063d2
Jacob <jacobmgevans@gmail.com>
2018-04-14 06:37:09 -0700

    In App I fixed some errors one included the wrong casing for a imported component

--- frontend/src/components/app/index.js
+++ frontend/src/components/app/index.js
@@ -1,30 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import React, { Component }from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
+import Dashboard from '../dashboard';
+import About from '../about';
 
-import Dashboard from '../dashboard'
-import About from '../about'
-
-if(true){
-    console.log('App component')
+if (true) {
+  console.log('App component');
 }
 
 class App extends Component {
   render() {
     return (
-
-      
       <Router>
-    <Switch>
-          <Route path='/' component={dashboard}/>
-         <Route path='/about' component={About}/>
-    </Switch>
+        <Switch>
+          <Route path="/" component={Dashboard} />
+          <Route path="/about" component={About} />
+        </Switch>
       </Router>
-    
     );
   }
 }

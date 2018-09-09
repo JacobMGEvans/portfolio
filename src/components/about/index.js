b3e0c6236c918fa35da6d6961128ae5c35ae113b
Jacob <jacobmgevans@gmail.com>
2018-09-09 12:06:44 -0700

    BABEL 7

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -18,6 +18,42 @@ export default class About extends PureComponent {
           <div>Summary and Bio will go here</div>
           using some anchor to the text or images.
         </div>
+
+        <div>
+          Icons made by
+          <a href="https://www.flaticon.com/authors/freepik" title="Twitter">
+            Twitter
+          </a>
+          from
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+          is licensed by
+          <a
+            href="http://creativecommons.org/licenses/by/3.0/"
+            title="Creative Commons BY 3.0"
+            target="_blank">
+            CC 3.0 BY
+          </a>
+        </div>
+
+        <div>
+          Icons made by
+          <a href="https://www.flaticon.com/authors/freepik" title="Linkedin">
+            Linkedin
+          </a>
+          from
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+          is licensed by
+          <a
+            href="http://creativecommons.org/licenses/by/3.0/"
+            title="Creative Commons BY 3.0"
+            target="_blank">
+            CC 3.0 BY
+          </a>
+        </div>
       </div>
     );
   }

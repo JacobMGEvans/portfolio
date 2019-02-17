8051f2c3d867974c88849b93cd9cb823d191358a
JacobMGEvans <jacobmgevans@gmail.com>
2019-02-16 22:13:43 -0800

    FORM data on submit

--- src/components/contact-form/index.js
+++ src/components/contact-form/index.js
@@ -9,38 +9,51 @@ const ContactForm = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
+    console.log(firstName, lastName, phone, email, comment);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="firstName"
-        placeholder="First Name!"
-        value={firstName}
-        onChange={e => setFirstName(e.target.value)}
-      />
-      <input
-        type="text"
-        name="lastName"
-        placeholder="Last Name!"
-        value={lastName}
-        onChange={e => setLastName(e.target.value)}
-      />
-      <input
-        type="text"
-        name="phone"
-        placeholder="Phone Number"
-        value={phone}
-        onChange={e => setPhone(e.target.value)}
-      />
-      <input
-        type="text"
-        name="email"
-        placeholder="Email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
+      <label htmlFor="first name">
+        First Name:
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name!"
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+        />
+      </label>
+      <label htmlFor="last name">
+        Last Name:
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name!"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+        />
+      </label>
+      Phone Number:
+      <label htmlFor="phone number">
+        <input
+          type="text"
+          name="phone"
+          placeholder="Phone Number"
+          value={phone}
+          onChange={e => setPhone(e.target.value)}
+        />
+      </label>
+      <label htmlFor="email">
+        Email:
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </label>
       <label htmlFor="Comment Text Area">
         Comment:
         <textarea value={comment} onChange={e => setComment(e.target.value)} />

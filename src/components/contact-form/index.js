5e505ed403a94174b0f2968866dc87209f71b4f4
Jacob Evans <cloud887@gmail.com>
2019-07-14 16:45:25 -0500

    portfolio

--- src/components/contact-form/index.js
+++ src/components/contact-form/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TextField } from "material-ui/TextField";
+import TextField from "@material-ui/core/TextField";
 import styles from "./style";
 
 const ContactForm = ({ styleProps }) => {
@@ -17,54 +17,50 @@ const ContactForm = ({ styleProps }) => {
   const { mainContainer } = styles;
   return (
     <form onSubmit={handleSubmit} style={{ ...styleProps, mainContainer }}>
-      <label htmlFor="firstname">
-        First Name:
-        <input
-          id="firstname"
-          type="text"
-          name="firstName"
-          placeholder="First Name!"
-          value={firstName}
-          onChange={e => setFirstName(e.target.value)}
-        />
-      </label>
-      <label htmlFor="lastname">
-        Last Name:
-        <input
-          id="lastname"
-          type="text"
-          name="lastName"
-          placeholder="Last Name!"
-          value={lastName}
-          onChange={e => setLastName(e.target.value)}
-        />
-      </label>
-      Phone Number:
-      <label htmlFor="phone-number">
-        <input
-          id="phone-number"
-          type="text"
-          name="phone"
-          placeholder="Phone Number"
-          value={phone}
-          onChange={e => setPhone(e.target.value)}
-        />
-      </label>
-      <label htmlFor="email">
-        Email:
-        <input
-          id="email"
-          type="text"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-      </label>
-      <label htmlFor="Comment Text Area">
-        Comment:
-        <textarea value={comment} onChange={e => setComment(e.target.value)} />
-      </label>
+      <TextField
+        label="First Name"
+        id="firstname"
+        type="text"
+        name="firstName"
+        placeholder="First Name!"
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+      />
+
+      <TextField
+        label="Last Name"
+        id="lastname"
+        type="text"
+        name="lastName"
+        placeholder="Last Name!"
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+      />
+      <TextField
+        label="Phone Number"
+        id="phone-number"
+        type="text"
+        name="phone"
+        placeholder="Phone Number"
+        value={phone}
+        onChange={e => setPhone(e.target.value)}
+      />
+      <TextField
+        label="example@gmail.com"
+        id="email"
+        type="text"
+        name="email"
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      <TextField
+        id="comment"
+        label="comment"
+        value={comment}
+        onChange={e => setComment(e.target.value)}
+      />
       <button type="submit" value="Submit">
         Submit!
       </button>
